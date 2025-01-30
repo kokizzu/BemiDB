762345014760dce457bd37b232e25e0d2aecc499
exAspArk <exaspark@gmail.com>
2025-01-30 09:23:39 -0500

    Automate downloading benchmark data for testing

--- benchmark/scripts/load-pg-data.sh
+++ benchmark/scripts/load-pg-data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+curl -L -o ./tpch.zip https://github.com/BemiHQ/BemiDB/releases/download/v0.1.0/TPC-H_generated_data_s0.1.zip
+unzip ./tpch.zip -d ./benchmark/data
+rm ./tpch.zip
+
 cd ./benchmark/data
+mv ./TPC-H_generated_data/* ./
+rm -rf ./TPC-H_generated_data
+rm -rf ./__MACOSX
 
 psql postgres://127.0.0.1:5432/tpch -f ./create-tables.ddl
 
